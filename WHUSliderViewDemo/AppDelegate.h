23e8d5d278b31fef9a90ef2f33c293bb2bd49cf1
tiger8888 <SuperNova@taivex02deMacBook-Pro.local>
2015-06-15 22:14:30 +0800

    Initial Commit

---
+++ WHUSliderViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHUSliderViewDemo
+//
+//  Created by SuperNova on 15/6/15.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHUSliderViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHUSliderViewDemo
+//
+//  Created by SuperNova on 15/6/15.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
