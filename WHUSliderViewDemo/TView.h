30864062c5a9e65b98b576ba06767524b4f9b01f
tiger8888 <SuperNova@taivex02deMacBook-Pro.local>
2015-06-24 14:16:05 +0800

    修复下顶部滚动过长的bug

---
+++ WHUSliderViewDemo/TView.h
@@ -0,0 +1,13 @@
+//
+//  TView.h
+//  WHUSliderViewDemo
+//
+//  Created by SuperNova on 15/6/24.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TView : UIView
+
+@end
---
+++ WHUSliderViewDemo/TView.m
@@ -0,0 +1,21 @@
+//
+//  TView.m
+//  WHUSliderViewDemo
+//
+//  Created by SuperNova on 15/6/24.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import "TView.h"
+
+@implementation TView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
